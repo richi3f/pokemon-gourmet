9220136b6f9ff887eb8b402dcd7439c53799ab4c
Ricardo Hernández Medina <richi3f@gmail.com>
2023-03-17 15:56:31 +0100

    :memo: Add docstrings to exceptions

--- src/pokemon_picnic/suggester/exceptions.py
+++ src/pokemon_picnic/suggester/exceptions.py
@@ -1,17 +1,27 @@
-__all__ = ["RepeatedPowers"]
+__all__ = [
+    "InvalidEffects",
+    "RepeatedPowers",
+    "UnexpectedPower",
+    "UnexpectedType"
+]
 
 
 class InvalidEffects(ValueError):
+    """Invalid combination of effects in a list of desired effects."""
     ...
 
 
 class RepeatedPowers(ValueError):
+    """Two or more Powers of the same type repeated in a list of desired
+    effects."""
     ...
 
 
 class UnexpectedPower(KeyError):
+    """Power not found."""
     ...
 
 
 class UnexpectedType(KeyError):
+    """Pokémon Type not found."""
     ...
