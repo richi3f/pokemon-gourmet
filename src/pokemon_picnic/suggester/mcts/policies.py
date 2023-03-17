fc564098f1381577bf49df4dd65862b580f52be5
Ricardo Hernández Medina <richi3f@gmail.com>
2023-03-17 13:19:10 +0100

    :sparkles: Make prob of short recipe policy configurable

--- src/pokemon_picnic/suggester/mcts/policies.py
+++ src/pokemon_picnic/suggester/mcts/policies.py
@@ -6,7 +6,7 @@ from typing import Callable
 from pokemon_picnic.suggester.mcts.action import Action, FinishSandwich
 from pokemon_picnic.suggester.mcts.state import Sandwich, State
 
-RolloutPolicy = Callable[[State], Action]
+RolloutPolicy = Callable[..., Action]
 
 
 def random_rollout_policy(state: State) -> Action:
@@ -16,15 +16,24 @@ def random_rollout_policy(state: State) -> Action:
     return random.choice(possible_actions)
 
 
-def short_recipe_rollout_policy(state: State) -> Action:
+def short_recipe_rollout_policy(state: State, prob: float = 0.5) -> Action:
     """A rollout policy that favors short recipes (i.e., the action to finish
-    the sandwich has higher probability of being picked)."""
+    the sandwich has higher probability of being picked).
+
+    Args:
+        state: Current state
+        prob: Probability. Chance to select the finish a sandwich action.
+
+    Raises:
+        ValueError: When `prob` is not between 0 and 1.
+    """
     possible_actions = state.get_possible_actions()
     try:
         finish_idx = possible_actions.index(FinishSandwich())
     except ValueError:
         return random.choice(possible_actions)
-    prob = 0.5
+    if not 0.0 <= prob <= 1.0:
+        raise ValueError("Probability must be between 0 and 1.")
     weights = [1.0] * len(possible_actions)
     weights[finish_idx] = prob / (1 - prob) * (len(possible_actions) - 1)
     return random.choices(possible_actions, weights, k=1)[0]
