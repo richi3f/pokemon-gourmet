e034e6cddc1f7fce8c03d11878176ccd90fe5d5e
Ricardo Hernández Medina <richi3f@gmail.com>
2023-03-17 13:09:29 +0100

    :sparkles: Add GUI console script

---
+++ src/pokemon_picnic/suggester/st.py
@@ -0,0 +1,15 @@
+import sys
+from pathlib import Path
+
+from streamlit.web import cli
+
+
+def main():
+    file_path = Path(__file__).parent / "gui.py"
+    args = ["streamlit", "run", str(file_path)]
+    sys.argv = args
+    sys.exit(cli.main())
+
+
+if __name__ == "__main__":
+    main()
