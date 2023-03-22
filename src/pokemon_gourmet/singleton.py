30e247e1f8107f457605b36b0b9e1ad4d36671d6
Ricardo Hernández Medina <richi3f@gmail.com>
2023-03-22 20:39:40 +0100

    :recycle: Move singleton to its own module

---
+++ src/pokemon_gourmet/singleton.py
@@ -0,0 +1,13 @@
+__all__ = ["Singleton"]
+
+
+from typing import Any, ClassVar
+
+
+class Singleton(type):
+    _instances: ClassVar[dict[type, Any]] = {}
+
+    def __call__(cls, *args: Any, **kwargs: Any) -> Any:
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
